f009ecf9c9e3931e50f9e6faf819462451ba7bb4
Виктор Локтев <viktor.loktev@mineral-info.ru>
2022-02-28 15:25:25 +0300

    Version bump

--- ExifName/Properties/AssemblyInfo.cs
+++ ExifName/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "2.22.01.01" )]
-[assembly: AssemblyFileVersion( "2.22.01.01" )]
+[assembly: AssemblyVersion( "22.02.28.01" )]
+[assembly: AssemblyFileVersion( "22.02.28.01" )]
